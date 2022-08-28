5b5d6476b74c52c4ad8ad549616a2769da5db103
PabloZavi <pablozaviok@gmail.com>
2022-08-28 13:28:19 -0300

    Subo front TP5 sin ABM

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,26 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
+
+
+import { BrowserRouter } from 'react-router-dom';
+import { Productos } from './componentes/Productos';
+import AppRutas from './AppRutas';
+
+//// <App /> <AppRutas /> <AppCocina />
+//En la línea 16 definimos qué archivo tsx queremos que inice, podemos usar App (App.tsx --> contador y login)
+//o AppCocina (AppCocina.tsx --> delivery)
+ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <BrowserRouter basename="/">
+      
+    <AppRutas /> 
+    </BrowserRouter>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
